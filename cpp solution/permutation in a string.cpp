39cd8ed659d2ad3a51545428abce3c6a57ddd458
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-03-23 21:17:49 +0600

    Update permutation in a string.cpp

--- cpp solution/permutation in a string.cpp
+++ cpp solution/permutation in a string.cpp
@@ -44,3 +44,63 @@ public:
         return false;
     }
 };
+// using hashmap
+// time: O(l1 + (l2 - l1) and space: O(1)
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size()){
+            return false;
+        }
+        unordered_map<char, int> pattern_freq;
+        for (int i = 0; i < s1.size(); i++){
+            if (pattern_freq.count(s1[i])){
+                pattern_freq[s1[i]] += 1;
+            }
+            else {
+                pattern_freq[s1[i]] = 1;
+            }
+        }
+        int window_size = s1.size();
+        // unique character count of s1
+        int uniqueCharCount = pattern_freq.size();
+        int left = 0;
+        // treverse s2 for the first window
+        while (left < window_size){
+             if (pattern_freq.count(s2[left])){
+                 pattern_freq[s2[left]] -= 1;
+                 if (pattern_freq[s2[left]] == 0){
+                     uniqueCharCount--;
+                 }
+             }
+             left++;
+        }
+        if (uniqueCharCount == 0){
+            return true;
+        }
+        int right = left;
+        left = 0;
+        while (right < s2.size()){
+            // removing first character which is out of window
+            if (pattern_freq.count(s2[left])){
+                pattern_freq[s2[left]] += 1;
+                if (pattern_freq[s2[left]] == 1){
+                    uniqueCharCount++;
+                }
+            }
+            if (pattern_freq.count(s2[right])){
+                pattern_freq[s2[right]] -= 1;
+                if (pattern_freq[s2[right]] == 0){
+                    uniqueCharCount--;
+                }
+            }
+            if (uniqueCharCount == 0){
+                return true;
+            }
+            left++;
+            right++;
+            
+        }
+        return false;
+    }
+};
