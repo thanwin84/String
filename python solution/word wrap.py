647985b47ff376db78e9a1b26bcdb878141b55af
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-04-01 21:31:14 +0600

    Create word wrap.py

---
+++ python solution/word wrap.py
@@ -0,0 +1,31 @@
+# time: O(n^2) and space: O(n) 
+def solve(self, nums, k, pos, dp):
+    lastIndex = len(nums) - 1
+    # we cannot have extra space at the last line
+    if pos == lastIndex:
+        return 0
+    ans = float('inf')
+    _sum = 0
+    if dp[pos] != -1:
+        return dp[pos]
+    for currentIndex in range(pos, len(nums)):
+        _sum += nums[currentIndex]
+        widthCovered = _sum + currentIndex - pos
+        if widthCovered <= k:
+            if currentIndex == lastIndex:
+                ans = 0
+                break
+            extraSpaces = k - widthCovered
+            right = self.solve(nums, k, currentIndex + 1, dp)
+            ans = min(extraSpaces ** 2 + right, ans)
+        else:
+          # we can't keep words in the same line until this positon
+          # so we can't extend our search
+            break
+    dp[pos] = ans
+    return ans
+
+    def solveWordWrap(self, nums, k):
+        # Code here
+        dp = [-1 for i in range(len(nums) + 1)]
+        return self.solve(nums, k, 0, dp)
